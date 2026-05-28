e0b78498185be84b19a0133a07120a4a41016ca6
Miguel Loranca <mloranca703@gmail.com>
2026-05-28 16:30:26 -0600

    made a simulation on how the stack in a computer works

--- stack_simulator/main.c
+++ stack_simulator/main.c
@@ -1,80 +1,93 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#define STACK_NUM_BYTES 26
+#define STACK_BYTES 26
+
+struct Allocated_bytes_arr {
+    size_t bytes[STACK_BYTES];
+    size_t bytes_count;
+};
 
 struct Stack {
-    int items[STACK_NUM_BYTES];
-    unsigned int stack_origin;
-    unsigned int stack_ptr;
-
-    int bytes_allocated[STACK_NUM_BYTES];
-    unsigned int bytes_allocated_pos;
+    uint8_t items[STACK_BYTES];
+    uint8_t *stack_origin;
+    size_t stack_ptr;
+    struct Allocated_bytes_arr ab_arr;
 };
 
 void stack_init(struct Stack *s);
-void stack_alloc(struct Stack *s, unsigned int bytes, int value);
+void stack_alloc(struct Stack *s, size_t bytes, uint8_t val);
 void stack_dealloc(struct Stack *s);
-void stack_println(const struct Stack s);
+void stack_println(struct Stack s);
+void stack_println_full(struct Stack s);
 
 int main(void)
 {
-    struct Stack stack = {0};
-    stack_init(&stack);
+    struct Stack s;
+    stack_init(&s);
 
-    stack_println(stack);
+    stack_println(s);
+    stack_alloc(&s, 3, 10);
+    stack_alloc(&s, 1, 2);
+    stack_alloc(&s, 2, 255);
+    stack_alloc(&s, 2, 23);
+    stack_alloc(&s, 5, 145);
+    stack_alloc(&s, 3, 45);
+    stack_println(s);
 
-    stack_alloc(&stack, 2, 10);
-    stack_println(stack);
+    stack_dealloc(&s);
+    stack_dealloc(&s);
+    stack_println(s);
+    stack_println_full(s);
 
-    stack_alloc(&stack, 1, 3);
-    stack_println(stack);
+    stack_alloc(&s, 2, 255);
+    stack_println(s);
 
-    stack_alloc(&stack, 4, 345);
-    stack_println(stack);
-
-    stack_dealloc(&stack);
-    stack_println(stack);
-
-    stack_alloc(&stack, 3, 3);
-    stack_println(stack);
+    stack_println_full(s);
 
     return 0;
 }
 
 void stack_init(struct Stack *s)
 {
-    for (size_t i = 0; i < (size_t)STACK_NUM_BYTES; ++i) {
+    for (size_t i = 0; i < (size_t)STACK_BYTES; ++i) {
         s->items[i] = 0;
-        s->bytes_allocated[i] = 0;
+        s->ab_arr.bytes[i] = 0;
     }
 
-    s->stack_origin = (unsigned int)0;
-    s->stack_ptr = (unsigned int)0;
-    s->bytes_allocated_pos = 0;
+    s->ab_arr.bytes_count = 0;
+
+    s->stack_origin = &s->items[0];
+    s->stack_ptr = 0;
 }
 
-void stack_alloc(struct Stack *s, unsigned int bytes, int value)
+void stack_alloc(struct Stack *s, size_t bytes, uint8_t val)
 {
-    for (size_t i = 0; i < (size_t)bytes; ++i) {
-        s->items[s->stack_ptr++] = value;
+    for (size_t i = 0; i < bytes; ++i) {
+        s->items[s->stack_ptr++] = val;
     }
 
-    s->bytes_allocated[s->bytes_allocated_pos++] = bytes;
+    s->ab_arr.bytes[s->ab_arr.bytes_count++] = bytes;
 }
 
 void stack_dealloc(struct Stack *s)
 {
-    printf("stakc ptr: %d\n", s->stack_ptr);
-    printf("stakc bytes pos: %d\n", s->bytes_allocated_pos);
-    for (size_t i = s->stack_ptr; i < s->bytes_allocated_pos; ++i) {
-        s->items[i] = 0;
-    }
+    s->stack_ptr -= s->ab_arr.bytes[s->ab_arr.bytes_count - 1];
+    s->ab_arr.bytes_count--;
 }
 
-void stack_println(const struct Stack s)
+void stack_println_full(struct Stack s)
 {
-    for (size_t i = 0; i < (size_t)STACK_NUM_BYTES; ++i) {
+    for (size_t i = 0; i < (size_t)STACK_BYTES; ++i) {
+        printf("%d ", s.items[i]);
+    }
+    printf("\n");
+}
+
+void stack_println(struct Stack s)
+{
+    for (size_t i = 0; i < s.stack_ptr; ++i) {
         printf("%d ", s.items[i]);
     }
     printf("\n");
