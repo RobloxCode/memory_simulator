e4466c409bfe1d4f1816b67b4e9e23fb238cfda4
Miguel Loranca <mloranca703@gmail.com>
2026-08-08 22:51:06 -0600

    added the basic struct and funtions

--- memory_arena/arena.c
+++ memory_arena/arena.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Arena {
-    int8_t *data;
+typedef struct {
     size_t len;
-};
+    int8_t *data;
+    int8_t *pos;
+} Arena;
+
+void arena_init(Arena *a);
+void *arena_malloc(Arena *a, size_t nmemb, size_t size);
+void arena_deinit(Arena **a);
 
 int main(void) {
     puts("thue");
