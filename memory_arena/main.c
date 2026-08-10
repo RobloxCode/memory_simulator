4a699c6c6d8ed3fbdc45baae6f4977e4c993fae4
Miguel Loranca <mloranca703@gmail.com>
2026-08-09 20:42:29 -0600

    added a wrapper to malloc

--- memory_arena/main.c
+++ memory_arena/main.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *arena_xmalloc(Arena *a, size_t nmemb, size_t s);
+
 int main(void) {
     Arena a;
     Arena *aptr = &a;
     arena_init(&a);
 
     size_t nums_len = 20;
-    int32_t *nums = arena_malloc(aptr, nums_len, sizeof *nums);
-    if (!nums) {
-        return EXIT_FAILURE;
-    }
+    int32_t *nums = arena_xmalloc(aptr, nums_len, sizeof *nums);
 
     for (size_t i = 0; i < nums_len; ++i) {
         nums[i] = (int32_t)i;
@@ -24,7 +23,7 @@ int main(void) {
     }
 
     size_t nums2_len = 5;
-    int64_t *nums2 = arena_malloc(aptr, nums2_len, sizeof *nums2);
+    int64_t *nums2 = arena_xmalloc(aptr, nums2_len, sizeof *nums2);
 
     for (size_t i = 0; i < nums2_len; ++i) {
         nums2[i] = (int64_t)i + 1;
@@ -37,4 +36,14 @@ int main(void) {
 
     arena_deinit(&aptr);
     return EXIT_SUCCESS;
+}
+
+void *arena_xmalloc(Arena *a, size_t nmemb, size_t s) {
+    void *nums = arena_malloc(a, nmemb, s);
+    if (!nums) {
+        fprintf(stderr, "malloc failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return nums;
 }
