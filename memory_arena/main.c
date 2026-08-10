ed62f375b8a8cc0daaa16ed5b550c56611a831a2
Miguel Loranca <mloranca703@gmail.com>
2026-08-09 19:23:39 -0600

    added one more test

--- memory_arena/main.c
+++ memory_arena/main.c
@@ -8,17 +8,31 @@ int main(void) {
     Arena *aptr = &a;
     arena_init(&a);
 
-    int32_t *nums = arena_malloc(aptr, 2, sizeof *nums);
+    size_t nums_len = 20;
+    int32_t *nums = arena_malloc(aptr, nums_len, sizeof *nums);
     if (!nums) {
         return EXIT_FAILURE;
     }
 
-    for (size_t i = 0; i < 2; ++i) {
+    for (size_t i = 0; i < nums_len; ++i) {
         nums[i] = (int32_t)i;
     }
 
-    for (size_t i = 0; i < 2; ++i) {
+    printf("nums\n");
+    for (size_t i = 0; i < nums_len; ++i) {
         printf("%d ", nums[i]);
+    }
+
+    size_t nums2_len = 5;
+    int64_t *nums2 = arena_malloc(aptr, nums2_len, sizeof *nums2);
+
+    for (size_t i = 0; i < nums2_len; ++i) {
+        nums2[i] = (int64_t)i + 1;
+    }
+
+    printf("nums\n");
+    for (size_t i = 0; i < nums2_len; ++i) {
+        printf("%ld ", nums2[i]);
     }
 
     arena_deinit(&aptr);
