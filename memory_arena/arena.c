0cb2f484b5bdc7b7b033451f93b77fb155586a16
Miguel Loranca <mloranca703@gmail.com>
2026-08-09 18:38:08 -0600

    implemented basic functions

--- memory_arena/arena.c
+++ memory_arena/arena.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARENA_DEF_SIZE 1024
+
 typedef struct {
     size_t len;
     int8_t *data;
@@ -13,5 +15,28 @@ void *arena_malloc(Arena *a, size_t nmemb, size_t size);
 void arena_deinit(Arena **a);
 
 int main(void) {
+    Arena a;
+    Arena *aptr = &a;
+    arena_init(&a);
+
+    arena_deinit(&aptr);
     return 0;
+}
+
+void arena_init(Arena *a) {
+    a->data = malloc(ARENA_DEF_SIZE * sizeof *a->data);
+    if (!a->data) {
+        fprintf(stderr, "Error internal malloc\n");
+        return;
+    }
+
+    a->pos = a->data;
+    a->len = 0;
+}
+
+void arena_deinit(Arena **a) {
+    free((*a)->data);
+    (*a)->data = NULL;
+    (*a)->pos = NULL;
+    *a = NULL;
 }
